00c76a50f607ed7a4ade6506866780a0864b3027
hemantbeast <hemantbeast@gmail.com>
2017-09-24 23:40:51 +0530

    Revert "bluetooth: Disable CLOCK_BOOTTIME_ALARM"
    This reverts commit 68ad1ac94c5c5f4da24e866206f3fe6220653312.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,8 +21,4 @@
 #define BTM_DEF_LOCAL_NAME   "Xiaomi HM 1S"
 
 #define BLUETOOTH_QTI_SW TRUE
-
-/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
-#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
-
 #endif

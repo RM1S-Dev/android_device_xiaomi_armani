20ad714855448a048821e58eea31ab877398a7de
Hemant Sharma <hemantbeast@gmail.com>
2017-12-29 22:03:18 +0530

    armani: Include legacy qcom sepolicy rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_NO_SECURE_DISCARD := true
 
 # SELinux
+include device/qcom/sepolicy/legacy-sepolicy.mk
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 

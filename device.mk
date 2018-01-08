3680a0e0b375c5d305598ac9f19dda4781fb1ff8
Hemant Sharma <hemantbeast@gmail.com>
2018-01-08 15:32:55 +0530

    armani: enable lineage overlay
     * disable unused overlay

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-#    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi

59d98fbb47f1573ea5d2c619610a1fdf988daa2a
Hemant Sharma <hemantbeast@gmail.com>
2018-01-16 11:37:33 +0530

    armani: Update dexpreopt flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,14 +58,7 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 WITH_LINEAGE_CHARGER := false
 
 # Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true

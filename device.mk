2b7c87529b4e90f72bd51652ff0605409cbbe52d
Hemant Sharma <hemantbeast@gmail.com>
2018-05-12 16:59:44 +0530

    armani: Build tinyxml
    Change-Id: Icaeb50cb63b45fff5004bbcb6252e780b86f5c4b

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
+# Tools
+PRODUCT_PACKAGES += \
+    libtinyxml
+
 # WCNSS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \

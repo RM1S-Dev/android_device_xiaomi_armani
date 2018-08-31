c3deac74bfc2bf6ec9e7f0186d52483c3b79b0be
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:34:18 +0200

    dior: Load dummy initial BT MAC address.
    The real one will be loaded by hci_qcomm_init, but the BT HAL wants an
    address to be set before that point.
    
    Change-Id: Ia8ff499ccaa862ad697dcfecb969b29f29ee89f4

--- init/init_armani.cpp
+++ init/init_armani.cpp
@@ -58,6 +58,8 @@ void property_override_dual(char const system_prop[],
 
 void vendor_load_properties()
 {
+    // Init a dummy BT MAC address, will be overwritten later
+    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
     std::string platform = GetProperty("ro.board.platform", "");
     if (platform != ANDROID_TARGET)
         return;

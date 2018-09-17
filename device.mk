c1ff15eeb2c62e036c7f61e8606e574c8b2405df
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    dior: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- device.mk
+++ device.mk
@@ -234,7 +234,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Ramdisk
 PRODUCT_PACKAGES += \

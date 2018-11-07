c46d6bacc7377da30d471856e38875e059bdb32f
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    armani: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- device.mk
+++ device.mk
@@ -249,8 +249,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Power
 PRODUCT_PACKAGES += \

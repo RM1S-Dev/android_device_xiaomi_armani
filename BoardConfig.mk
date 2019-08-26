0ed1a8c79e103c39d710fe77681916a5604571f6
subdragonzj <zj502407679@gmail.com>
2019-08-26 23:59:56 +0800

    armani: set TARGET_PROCESS_SDK_VERSION_OVERRIDE 22

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,10 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 TARGET_USES_NON_TREBLE_CAMERA := true
+TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
+    /system/bin/cameraserver=22 \
+    /system/bin/mediaserver=22 \
+    /system/vendor/bin/mm-qcamera-daemon=22
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true

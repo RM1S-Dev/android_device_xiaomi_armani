09f53f27043e49550faad390c409cb70fa2cae44
bohu <bohu@google.com>
2019-10-26 12:56:00 +0100

    armani: Disable codec2
    BUG: 133178866
    Change-Id: Ie6698604357adfb7c36d6655ea8a42aecad6b219

--- system_prop.mk
+++ system_prop.mk
@@ -67,6 +67,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.ccodec=0 \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
     drm.service.enabled=1 \

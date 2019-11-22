6af264c8aa38049928c8a09873c9f9ec6efef5ec
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    armani: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- device.mk
+++ device.mk
@@ -60,10 +60,9 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 

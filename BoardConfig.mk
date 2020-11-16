9df9a161a45aead9d1549147055cd16913652b50
TARKZiM <tom8476oo@gmail.com>
2020-11-16 21:27:42 +0800

    armani: Revert to old persist partition layout

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,12 +174,9 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_NO_SECURE_DISCARD := true
 
-# Root
-BOARD_ROOT_EXTRA_SYMLINKS := \
-    /mnt/vendor/persist:/persist
-
 BOARD_ROOT_EXTRA_FOLDERS := \
     /firmware \
+    /persist \
     /tombstones
 
 # SELinux

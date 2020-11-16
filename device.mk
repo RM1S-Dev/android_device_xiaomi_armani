520acf8160d9c06673db1f26a2a1e5f1ce898122
TARKZiM <tom8476oo@gmail.com>
2020-11-16 11:00:37 +0800

    Revert "armani: Setup device soong namespace"
    This reverts commit 711c76492c7bc6306f0c8be1446b20275ca4ca6e.

--- device.mk
+++ device.mk
@@ -248,10 +248,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl.armani \
     android.hardware.sensors@1.0-service.armani
 
-# Soong
-PRODUCT_SOONG_NAMESPACES += \
-    device/xiaomi/armani
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8226.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8226.conf

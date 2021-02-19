06de9543ee5231338f011be23205ab2114bb42de
TARKZiM <tom8476oo@gmail.com>
2021-02-20 02:32:46 +0800

    armani: Remove unused inherit target

--- device.mk
+++ device.mk
@@ -25,8 +25,6 @@ PRODUCT_AAPT_PREBUILT_DPI := xhdpi 280dpi hdpi tvdpi mdpi ldpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
